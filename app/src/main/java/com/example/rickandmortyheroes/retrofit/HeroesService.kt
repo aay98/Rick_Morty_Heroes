4a80407b3264ffb6a867cadddd21f900dc99179a
andrey <aay9898@gmail.com>
2022-04-15 17:11:06 +0300

    Избавление от ошибок

--- app/src/main/java/com/example/rickandmortyheroes/retrofit/HeroesService.kt
+++ app/src/main/java/com/example/rickandmortyheroes/retrofit/HeroesService.kt
@@ -1,12 +1,13 @@
 package com.example.rickandmortyheroes.retrofit
 
+import Json4Kotlin_Base
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface HeroesService {
     @GET("data/api/character")
-    fun getAllHaracters(
-    ): Call<HeroesResponse>
+    fun getAllCharacters(
+    ): Call<Json4Kotlin_Base>
 
 }
--- app/src/main/java/com/example/rickandmortyheroes/retrofit/RetrofitGetResponse.kt
+++ app/src/main/java/com/example/rickandmortyheroes/retrofit/RetrofitGetResponse.kt
@@ -1,11 +1,21 @@
 package com.example.rickandmortyheroes.retrofit
 
 import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitGetResponse {
     private val baseUrl =
         "http://api.openweathermap.org/"
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val service: HeroesService = retrofit.create(HeroesService::class.java)
     fun getCharacter(errorLiveData: MutableLiveData<String>){
-        val call = ser
+        val call = service.getAllHaracters()
     }
 }
