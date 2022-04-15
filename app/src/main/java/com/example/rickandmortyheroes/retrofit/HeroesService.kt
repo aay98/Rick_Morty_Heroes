a8b9783c47e41c870462ad3359e22a366529388d
andrey <aay9898@gmail.com>
2022-04-15 17:36:54 +0300

    закончил с ретрофит гет респонс

--- app/src/main/java/com/example/rickandmortyheroes/retrofit/HeroesService.kt
+++ app/src/main/java/com/example/rickandmortyheroes/retrofit/HeroesService.kt
@@ -1,6 +1,7 @@
 package com.example.rickandmortyheroes.retrofit
 
 import Json4Kotlin_Base
+import Results
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,5 +10,9 @@ interface HeroesService {
     @GET("data/api/character")
     fun getAllCharacters(
     ): Call<Json4Kotlin_Base>
+    @GET("data/api/character/")
+    fun getInfo(
+        @Query("id") id: String
+    ): Call<Results>
 
 }
--- app/src/main/java/com/example/rickandmortyheroes/retrofit/RetrofitGetResponse.kt
+++ app/src/main/java/com/example/rickandmortyheroes/retrofit/RetrofitGetResponse.kt
@@ -1,5 +1,8 @@
 package com.example.rickandmortyheroes.retrofit
 
+import Json4Kotlin_Base
+import Results
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import retrofit2.Call
 import retrofit2.Callback
@@ -15,7 +18,73 @@ class RetrofitGetResponse {
         .addConverterFactory(GsonConverterFactory.create())
         .build()
     private val service: HeroesService = retrofit.create(HeroesService::class.java)
-    fun getCharacter(errorLiveData: MutableLiveData<String>){
-        val call = service.getAllHaracters()
+    private val successfulResponseCod = 200
+    private val units =
+        "metric"
+    private val exclude =
+        "current,minutely,alerts"
+    private val responseError = "Error in response body"
+    private val codError = "Error in response cod"
+
+    fun getResponse(errorLiveData: MutableLiveData<String>,callback: (oneCall: Json4Kotlin_Base?) -> Unit){
+        val call = service.getAllCharacters()
+        call.enqueue(object : Callback<Json4Kotlin_Base> {
+            override fun onResponse(
+                call: Call<Json4Kotlin_Base>?,
+                response: Response<Json4Kotlin_Base>?
+            ) {
+                Log.d("MyTag", "resp cod is ${response!!.code()}")
+                Log.d("MyTag", "resp is $response")
+                if (response.code() == successfulResponseCod && response.body() != null) {
+                    val returnResponse = response.body()
+                    callback(returnResponse)
+                } else {
+                    if (response.code() == successfulResponseCod)
+                        errorLiveData.value = responseError
+                    else
+                        errorLiveData.value = codError
+                    callback(null)
+                }
+            }
+
+            override fun onFailure(call: Call<Json4Kotlin_Base>?, t: Throwable?) {
+                if (t != null) {
+                    errorLiveData.value = t.message.toString()
+                }
+                callback(null)
+            }
+
+        })
+    }
+
+    fun getResponseInfo(errorLiveData: MutableLiveData<String>,id : String ,callback: (oneCall: Results?) -> Unit){
+        val call = service.getInfo(id)
+        call.enqueue(object : Callback<Results> {
+            override fun onResponse(
+                call: Call<Results>?,
+                response: Response<Results>?
+            ) {
+                Log.d("MyTag", "resp cod is ${response!!.code()}")
+                Log.d("MyTag", "resp is $response")
+                if (response.code() == successfulResponseCod && response.body() != null) {
+                    val returnResponse = response.body()
+                    callback(returnResponse)
+                } else {
+                    if (response.code() == successfulResponseCod)
+                        errorLiveData.value = responseError
+                    else
+                        errorLiveData.value = codError
+                    callback(null)
+                }
+            }
+
+            override fun onFailure(call: Call<Results>?, t: Throwable?) {
+                if (t != null) {
+                    errorLiveData.value = t.message.toString()
+                }
+                callback(null)
+            }
+
+        })
     }
 }
